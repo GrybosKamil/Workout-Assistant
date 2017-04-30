7875291f45f9ab8064eab606538ec888cd139e58
Paulina <pk.koziol49@gmail.com>
2017-04-30 23:58:51 +0200

    working parallax scrolling

--- public/app/main/script.js
+++ public/app/main/script.js
@@ -1,4 +1,10 @@
-$('.parallax-window')
-    .parallax({
-        imageSrc: 'https://newsbook.pl/wp-content/uploads/2016/06/sport.jpg'
-    });
+$(document).ready(function () {
+    $('.parallax-1')
+        .parallax({
+            imageSrc: 'https://newsbook.pl/wp-content/uploads/2016/06/sport.jpg'
+        });
+    $('.parallax-2')
+        .parallax({
+            imageSrc: 'http://watchfit.com/wp-content/uploads/2015/05/Male-Body-Shapes-Training-Tips1.jpg'
+        });
+});
