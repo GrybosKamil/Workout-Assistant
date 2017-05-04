b58a0af5a035eeaf6ac75bfff4e5cd64ac990538
Paulina <pk.koziol49@gmail.com>
2017-05-04 23:10:12 +0200

    parallax scrolling

---
+++ public/app/components/main/main.component.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function () {
+    angular.module('main')
+        .component('main', {
+            templateUrl: 'app/components/main/main.template.html',
+            controller: function Main() {
+            }
+        });
+})();
