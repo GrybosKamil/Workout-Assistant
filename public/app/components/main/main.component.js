24756be8701eb2689ec0a28b86026f98502b2777
Kamil Gryboś <grybos.kamil@gmail.com>
2017-05-05 17:38:52 +0200

    Change parallax scrolling

--- public/app/components/main/main.component.js
+++ public/app/components/main/main.component.js
@@ -5,6 +5,34 @@
         .component('main', {
             templateUrl: 'app/components/main/main.template.html',
             controller: function Main() {
+                var self = this;
+
+                this.parallaxBackgrounds = [{
+                    parallax_number: ".parallax-1",
+                    imageSrc: 'https://newsbook.pl/wp-content/uploads/2016/06/sport.jpg'
+
+                }, {
+                    parallax_number: ".parallax-2",
+                    imageSrc: 'http://watchfit.com/wp-content/uploads/2015/05/Male-Body-Shapes-Training-Tips1.jpg'
+                }];
+
+                this.addParallaxBackground = function (parallaxBackground) {
+                    $(parallaxBackground.parallax_number)
+                        .parallax({
+                            imageSrc: parallaxBackground.imageSrc
+                        });
+                };
+
+                this.initParallax = function () {
+                    console.log("InitParalax");
+
+                    $(function () {
+                        self.parallaxBackgrounds
+                            .forEach(self.addParallaxBackground);
+                    })
+                };
+
+                this.initParallax();
             }
         });
 })();
