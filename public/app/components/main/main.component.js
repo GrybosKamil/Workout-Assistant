212f9ef0317d80c425d7f99409d34d46be4ecfd9
Kamil Gryboś <grybos.kamil@gmail.com>
2017-05-05 18:32:56 +0200

    Use https for images

--- public/app/components/main/main.component.js
+++ public/app/components/main/main.component.js
@@ -8,16 +8,15 @@
                 var self = this;
 
                 this.parallaxBackgrounds = [{
-                    parallax_number: ".parallax-1",
+                    parallax_number: "0",
                     imageSrc: 'https://newsbook.pl/wp-content/uploads/2016/06/sport.jpg'
-
                 }, {
-                    parallax_number: ".parallax-2",
-                    imageSrc: 'http://watchfit.com/wp-content/uploads/2015/05/Male-Body-Shapes-Training-Tips1.jpg'
+                    parallax_number: "1",
+                    imageSrc: 'https://www.freeletics.com/en/knowledge/wp-content/uploads/sites/12/2016/03/box-1@2x@2x.jpg'
                 }];
 
                 this.addParallaxBackground = function (parallaxBackground) {
-                    $(parallaxBackground.parallax_number)
+                    $(".parallax-" + parallaxBackground.parallax_number)
                         .parallax({
                             imageSrc: parallaxBackground.imageSrc
                         });
