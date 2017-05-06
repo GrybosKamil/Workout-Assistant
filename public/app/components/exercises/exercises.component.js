ad4101c1d6ebcf162526bc1eb35a4e420441fd98
Paulina <pk.koziol49@gmail.com>
2017-05-07 01:12:05 +0200

    start sorting

--- public/app/components/exercises/exercises.component.js
+++ public/app/components/exercises/exercises.component.js
@@ -6,9 +6,16 @@
             templateUrl: 'app/components/exercises/exercises.template.html',
             controller: function Exercises(Exercises) {
 
+                this.sortType = '';
+                this.sortingUsed = false;
+
                 Exercises.getExercises().then((response) => {
                     this.exercises = response.data;
                 });
+
+                this.changeSorting = function (newSorting) {
+                    this.sortType = newSorting;
+                }
 
 
             }
