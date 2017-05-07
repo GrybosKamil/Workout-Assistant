98cc57519fb8c04c2a555ea2686673f1ea9660a9
Paulina <pk.koziol49@gmail.com>
2017-05-07 18:17:21 +0200

    acs desc sorting

--- public/app/components/exercises/exercises.component.js
+++ public/app/components/exercises/exercises.component.js
@@ -11,12 +11,33 @@
                     descending: undefined
                 };
 
+                this.activeSorting = this.sorting.active;
+
                 Exercises.getExercises().then((response) => {
                     this.exercises = response.data;
                 });
 
                 this.changeSorting = function (newSorting) {
+
+                    if(this.sorting.active === newSorting){
+                        this.sorting.descending = !this.sorting.descending;
+                    } else {
+                        this.sorting.active = newSorting;
+                        this.sorting.descending = false;
+                    }
+
                     this.sorting.active = newSorting;
+                };
+
+
+                this.getIcon = function (column) {
+                    if(this.sorting.active === column) {
+                        return this.sorting.descending ?
+                            'glyphicon-triangle-top'
+                            : 'glyphicon-triangle-bottom';
+                    }
+
+                    return 'glyphicon-triangle-top';
                 }
 
             }
